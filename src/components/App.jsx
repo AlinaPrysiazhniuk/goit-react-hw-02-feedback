d9de690e317c319e4d0076ba931d85fa4674476d
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-06 17:50:33 +0300

    update files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,58 @@
-import React from "react";
-import Feedback from "./Feedback/Feedback";
+import React, { Component } from "react";
+import {Feedback} from "./Feedback/Feedback";
+import { Section } from "./Section/Section";
+import { Statistics } from "./Statistics/Statistics";
+import { Notification } from "./Notification/Notification";
 
-export const App = () => {
-  return (
-    <Feedback />
-  );
-};
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  }
+
+  updateCountFeedback = value => {
+    this.setState(prevState => ({ 
+      [value]: prevState[value] + 1 }
+      ));
+  };
+  countTotalFeedback = () =>
+    Object.values(this.state).reduce((acc, value) => acc + value, 0);
+
+  countPositiveFeedbackPercentage = () => {
+    return this.countTotalFeedback()
+      ? ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0)
+      : '0';
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <>
+      <Section title="Please leave feedback">
+      <Feedback 
+      options={this.state}
+      onFeedback={this.updateCountFeedback}/>
+      </Section>
+
+      {this.countTotalFeedback() ? (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+           ) : (
+            <Notification message="There is no feedback" />
+          )}
+
+      </>
+    )
+  }
+
+
+}
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,80 +1,26 @@
-import React, { Component } from "react";
-import css from "./Feedback.module.css"
+//import React, { Component } from "react";
+//import css from "./Feedback.module.css";
+import PropTypes from 'prop-types';
 
-class Feedback extends Component{
-state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0, //потрібно буде винести звідси 
-}
+export const Feedback = ({options, onFeedback}) => {
+const stateData = Object.keys(options);
 
-
-
-handleClickButtonGood = (event) => {
-//console.log(event.target);
-this.setState((prevState) => ({
-    good: prevState.good + 1,
-    total: prevState.total + 1,
-}))
-console.log(this.state.good);
-}
-
-
-
-handleClickButtonNeutral = (event) => {
-      //  console.log(event.target);
-        this.setState((prevState) => ({
-            neutral: prevState.neutral + 1,
-            total: prevState.total + 1,
-        }))
-        console.log(this.state.neutral);
-        }
-
-handleClickButtonBad = (event) => {
-            //console.log(event.target);
-            this.setState((prevState) => ({
-                bad: prevState.bad + 1,
-                total: prevState.total + 1,
-            }))
-
-            console.log(this.state.bad);
-            }
-
-// countTotalFeedback= () => {
-//     //const total = 
-// }
-
-render () {
     return (
         <div>
-            <p>Please leave feedback</p>
-            <button type="button" onClick={this.handleClickButtonGood}>Good</button>
-            <button type="button" onClick={this.handleClickButtonNeutral}>Neutral</button>
-            <button type="button" onClick={this.handleClickButtonBad}>Bad</button>
-
-            <p>Statictis</p>
-            <ul>
-                <li className={css.item}>Good
-                <p>{this.state.good}</p>
-                </li>
-                <li className={css.item}>Neutral
-                <p>{this.state.neutral}</p>
-                </li>
-                <li className={css.item}>Bad
-                    <p>{this.state.bad}</p>
-                </li>
-                <li className={css.item}>Total
-                    <p>{this.state.total}</p>
-                </li>
-                <li className={css.item}>Positive feedback
-                    <p>{this.state.positivFeedback}</p>
-                </li>
-            </ul>
+            {stateData.map(option => (
+                <button
+                key={option}
+                type="button"
+                name={option}
+                onClick={() => onFeedback(option)}
+                >{option}</button>
+            ))}
 
         </div>)
 }
 
-}
+Feedback.propTypes = {
+    options: PropTypes.object,
+    onFeedback: PropTypes.func,
+  };
 
-export default Feedback;
